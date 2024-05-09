d23f561821a02198605c5f1bc9cb8b83c9280642
mdmonir-hossain <monirsoftengr@gmail.com>
2024-05-09 15:42:36 +0600

    navbar started

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Nav from './Components/Nav';
 
 function App() {
   
 
   return (
     <>
-      <h1>Hyggex Digital Services</h1>
+      <Nav/>
     </>
   );
 }
